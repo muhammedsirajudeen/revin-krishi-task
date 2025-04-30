3244776d0095cf1125595df47980332b748d3010
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2025-05-01 02:28:50 +0530

    feat: added og graph

--- farm-dashboard/app/page.tsx
+++ farm-dashboard/app/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 
 export default function LandingPage() {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col justify-center w-full">
       {/* Header */}
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container flex h-16 items-center justify-between">
